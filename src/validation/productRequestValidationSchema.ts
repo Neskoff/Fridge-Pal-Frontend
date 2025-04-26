afc56259410afce74917cbe325b8750740856db6
Ilija Neskovic <ilija.neskovic@univers.com>
2025-04-26 19:06:04 +0200

    Add custom number input

--- src/validation/productRequestValidationSchema.ts
+++ src/validation/productRequestValidationSchema.ts
@@ -4,7 +4,10 @@ import { DateTime } from "luxon";
 const productRequestValidationSchema = yup.object({
   name: yup.string().required("Product name is required"),
   type: yup.string().required("Product type is required"),
-  quantity: yup.number().required("Product quantity is required"),
+  quantity: yup
+    .number()
+    .required("Product quantity is required")
+    .min(1, "Quantity must be greater than 0"),
   quantityUnit: yup.string().required("Product quantity unit is required"),
   storedDate: yup.mixed<DateTime>().required("Product stored date is required"),
   expiryDate: yup.mixed<DateTime>().required("Product expiry date is required"),
